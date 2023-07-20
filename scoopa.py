32555ebf8db4fb964aa3e98167bebd9ca643cc5c
jeremiah-toribio <jeremiahtoribiotx@gmail.com>
2023-07-19 21:35:39 -0500

    scoopa better functioning

--- scoopa.py
+++ scoopa.py
@@ -1,20 +1,12 @@
 ### welcome statement visual as well as menu options associated with user input and its variable ###
 
 print(f'  ___________________________________________________  \n | Welcome to Floppa bank | Access your account here | \n  --------------------------------------------------- \n | Please type 1-4 to select their respective option.| \n |            What would you like to do?             | \n |___________________________________________________|')
-print(f'\n [ 1 ] View current balance \n [ 2 ] Record a Debit (Deposit) \n [ 3 ] Record a Credit (Withdrawal) \n [ 4 ] Exit \n   ')
+print(f'\n [ a ] View current balance \n [ b ] Record a Debit (Deposit) \n [ c ] Record a Credit (Withdrawal) \n [ d ] Exit \n   \n')
 
-
-user_input = str(input( '\n' ))
 
 ### This is where user input reactions will be initialized ###
 #class Transactions:
 
-
-
-def user1():
-    account_balance = ''
-    print(f'Your current balance is: {account_balance}')
-    return
 
 def user2():
     return
@@ -22,34 +14,65 @@ def user2():
 def user3():
     return
 
-def user4():
-    print('Thanks for banking with big floppa')
+def exiter():
+    print('''
+          Thank you for banking with big floppa\n
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠐⣿⣿⣦⢻⣿⣿⡇⠀⠀⠀⠀⠀⠀⢸⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣤⣶⣿⠟⠁⢀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣽⣿⣿⡄⢻⣿⡇⠀⠀⠀⠀⠀⠀⢸⡁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣴⣶⣿⣿⠟⠁⠀⢀⡼
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠻⣿⢾⣧⣤⢿⡧⠀⠀⠀⠰⣤⡀⢈⠇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣠⣾⡿⢟⣵⣿⡟⠁⠀⠀⣴⠋⠀
+⣤⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣨⣤⣤⠿⠆⠀⠀⠀⠀⠀⠉⠛⠾⣧⣤⣀⣀⡀⠀⠀⢀⣠⣶⣿⣿⡟⢉⣴⣿⡿⠁⠀⠀⣠⠞⠁⠀⠀
+⣿⠇⠀⠀⠀⠀⠀⣀⣴⡿⠿⠋⠀⠀⠀⠀⠀⢀⣀⡀⣶⠂⠀⠀⠀⠻⣯⣭⡛⠶⣶⣿⣿⣿⣏⣡⣴⣿⡟⠉⠀⠀⢀⡼⠃⠀⠀⠀⠀
+⠏⠀⠀⠀⣀⣴⡾⠟⠛⠁⠀⠀⠀⠀⠀⠀⠚⢻⣿⣵⣏⠀⠀⠀⠀⠀⠘⢿⣇⠀⠀⠈⠻⢮⡛⣿⣿⠏⠀⠀⠀⢠⡏⠀⠀⠀⠀⠀⠀
+⠁⣀⠀⣾⠟⠋⠀⠀⠀⠀⣠⣾⣶⣤⣀⡀⢀⠈⠻⣿⣿⣹⡄⠀⠀⠀⠀⠘⣿⡀⠀⠀⠀⠈⢷⡌⠻⣦⠀⠀⠶⠟⠀⠀⠀⠀⠀⠀⠀
+⢠⣿⠟⠁⠀⠀⠀⢀⣼⣿⣿⡟⠉⠉⠉⠋⠈⠁⠒⣿⣿⣿⠀⠀⠀⠀⠀⠀⠈⣷⠀⠀⠀⠀⠈⣿⣧⠈⢷⣆⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⢿⣿⠄⠀⠠⠄⠴⣾⣿⣿⣥⣤⣤⣾⣿⣿⣟⣳⣀⢿⠀⠀⠀⠀⠀⠀⠀⠀⠀⢘⣧⠃⠀⠀⠀⣿⣿⡟⠀⣹⣦⠀⠀⠀⠀⠀⠀⠀⠀
+⢠⡇⠀⠀⠀⠀⢺⣿⡟⢿⣿⣿⣿⣿⣿⣿⣿⠿⣿⣿⣆⠀⠀⢄⠀⠀⠀⠀⠀⢹⣿⠀⠀⠀⠀⢿⣿⠟⠈⠟⠻⢷⡀⠀⠀⠀⠀⠀⠀
+⢿⣷⢠⣶⠆⠀⢸⢯⣤⠀⠛⢿⣿⣾⣿⣿⣿⣿⣿⣿⣿⡄⠀⠈⠇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣴⣦⣤⣤⡈⣻⡀⠀⠀⠀⠀⠀
+⢀⣿⣿⣿⣶⠇⠈⠈⠹⠧⣄⡀⠙⠲⢿⣿⣿⣿⣿⣿⣿⣷⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⣿⣯⣿⣿⣿⣿⣷⡄⠀⠀⠀⠀
+⣾⣿⣿⣿⣷⣦⠉⠀⠀⠀⠈⠉⠑⠂⠀⠈⠙⢿⣿⣿⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⠼⠂⠀⠀⠀⣿⣿⣿⣿⣿⣿⡟⠀⣿⣇⠀⠀⠀⠀
+⣿⣿⣿⣿⣿⣿⣇⠈⠀⠂⠀⠀⠀⠀⠀⠀⠀⠀⠻⣿⣿⣿⣿⣦⠀⠀⠀⠀⠀⣠⣼⣄⠀⠀⢸⣿⣮⣿⣿⠿⠋⠀⣰⣉⢸⡆⠀⠀⠀
+⠀⠀⠙⣿⣿⣿⣿⣷⣦⠀⣤⡀⠀⠀⠀⠀⠀⠀⠸⣿⣿⣿⣿⣿⠀⠀⠀⠀⠀⠉⠀⠈⠁⠁⣸⣿⣿⠇⠀⠠⠾⠛⠋⠀⠈⣿⠀⠀⠀
+⠀⠀⠀⠈⠻⣿⣿⣿⣿⣶⣸⣷⠀⠀⠀⠀⠀⠀⠀⢸⡿⣿⣿⣿⡆⠀⣦⡀⠀⠀⠀⠀⠀⠀⢹⣿⣧⣀⠀⠀⠀⠀⠀⠀⢀⣻⣆⠀⠀
+⠀⠀⠀⠀⠘⠿⠿⣿⣿⣿⣿⣿⣿⡶⢶⡿⠥⡄⠀⠀⠁⠙⣿⣿⣿⣆⣿⣿⣶⣤⣤⣀⣠⡄⠈⣿⣿⣇⠁⠀⠀⠀⠀⠀⠈⠉⢻⡄⠀
+⠂⠀⠀⠀⠀⠀⠀⠀⠈⠉⢿⣿⣿⣷⣿⣶⣄⣠⠀⠀⠀⠀⢸⣿⣿⣿⣿⣿⠁⠀⠉⠙⢿⡹⣿⣿⡏⠹⠆⠀⠀⠀⠀⠀⠀⠀⠀⢣⠀
+⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠰⣿⣿⣿⣿⣿⣿⣿⣄⡄⠀⠀⠀⠘⣿⣿⣿⣿⣧⠀⢰⠀⠈⣷⣾⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⣿⣿⣿⣿⣿⣁⡀⠀⠀⠀⠀⠀⠈⠛⢿⣿⣿⢀⣾⡇⣾⣿⠿⠁⠀⠀⠀⢀⣀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢻⣿⣿⣿⣿⣿⣿⣿⡧⠀⠀⠀⠀⠀⠈⢿⣿⣿⣿⣿⡿⠃⠀⠀⠀⠀⠀⠚⣿⠀⠀⠀⠀⠀⠀⠀⢠
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢻⣿⣿⣿⣿⣧⣤⣄⠀⠀⠀⠀⠀⠀⠘⣿⣿⡿⠋⠀⠀⠀⠀⠀⣀⣤⣴⣿⠀⠀⠀⠀⠀⠀⠀⢸
+⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠹⣿⣿⣿⣿⣿⣿⠿⠶⣶⣶⣶⠶⢀⣼⣿⡁⠰⣤⣄⡀⠀⠀⣈⣹⡿⠁⠀⣀⣠⣤⣀⣤⣤⣿
+⣷⣆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠙⠛⠛⢿⣿⡿⠟⠛⠻⠿⠛⠛⠋⠙⠻⢶⣬⣭⣍⣭⡿⠛⣉⣴⣶⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣇⣠⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣤⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣄⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⢫⣟⣹⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿
+⣿⣿⣿⣿⣿⣇⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣸⡆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⠟⠋⠁⠀
+          ''')
     return exit()
 
 ### Check to see if input is valid and routing to proper functions ###
 
 
-options = ['1','2','3','4']
-#def menu():
-#while user_input not in options:
+options = ['a','b','c','d']
+account_balance = ''
+
 
 attempts = 3
-
 while attempts != 0:
-    if user_input.isdigit() == True:
-        if user_input not in options:
-            attempts -= 1
-            print('Please select only a digit that is an option.')
-        elif user_input == '1':
-            user1()
-        elif user_input == '2':
-            user2()
-        elif user_input == '3':
-            user3()
-        elif user_input == '4':
-            user4()
-        else:
-            print('Only numbers.')
+    user_input = str(input())
+    if user_input not in options:
+        attempts -= 1
+        print(f'Please select only a letter that is an option. \nYou have {attempts} attempt left before you are removed.')
+    elif user_input == 'a':
+        print(f'Your current balance is: {account_balance}')
+
+        break
+    elif user_input == 'b':
+        print(f'How much would you like to deposit today? We are not accepting change.')
+        deposit = int(input())
+        break
+    elif user_input == 'c':
+        user3()
+        break
+    elif user_input == 'd':
+        exiter()
     else:
-        print('Only digits.')
+        print(f'Please select only a letter that is an option. \nYou have {attempts} attempt left before you are removed.')
         attempts -= 1
